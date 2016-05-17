ba2bb5c344f97fedff4c612430646ffb2b9b2955
z123 <z123@users.noreply.github.com>
2016-05-16 22:23:39 -0700

    Quotes are working

--- scripts/robinhood.py
+++ scripts/robinhood.py
@@ -1,13 +1,38 @@
 import click
+import requests
 
+class Robinhood:
+
+    api_base = 'https://api.robinhood.com'
+
+    def __init__(self):
+        self.session = requests.Session()
+
+    def login(self, username, password):
+        pass
+
+    def get_quote(self, symbol):
+        url = '%s/quotes/%s/' % (self.api_base, symbol.strip().upper())
+        r = self.session.get(url)
+        return r.json()
 
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    ctx.obj = Robinhood()
 
 @cli.command()
-def quote():
-    pass
+@click.argument('symbol')
+@click.pass_obj
+def quote(robinhood, symbol):
+    r = robinhood.get_quote(symbol)
+    price = float(r['last_trade_price'])
+    previous_close = float(r['previous_close'])
+    pct_change = (price - previous_close) / previous_close * 100
+    sign = '+' if pct_change > 0 else '-'
+    click.echo('Symbol: %s' % r['symbol'])
+    click.echo('Price: %.2f' % price)
+    click.echo('Percent Change: %s%.2f%%' % (sign, pct_change))
 
 @cli.command()
 def portfolio():
@@ -24,5 +49,3 @@ def buy():
 @cli.command()
 def sell():
     pass
-
-
